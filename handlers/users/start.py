83cdf77ede920a48d6501ef231b0f3953cb8e4bb
Mekhroj Akhatkulov <151901779+akhatkulov@users.noreply.github.com>
2024-07-06 09:01:40 +0300

    building

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,10 +1,23 @@
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
-from utils.db_api.alch import create_user
-from loader import dp
+from utils.db_api.alch import create_user,put_step
+from loader import dp,bot
+
+from keyboards.inline.user import home_menu,ads_yes_no
 
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(f"Salom, {message.from_user.full_name}!")
-    await create_user(cid=message.chat.id)
+    create_user(cid=message.chat.id)
+    await message.answer(f"Salom, {message.from_user.full_name}!",reply_markup=home_menu())
+
+@dp.callback_query_handler(lambda call: True)
+async def get_ads(call: types.CallbackQuery):
+    print(call)
+    if call.data == "put_ads":
+        print("-----")
+        await bot.send_message(chat_id=call.message.chat.id, text="E'lon joylamoqchimisiz?", reply_markup=ads_yes_no())
+    elif call.data == "yes_ads":
+        await bot.send_message(chat_id=call.message.chat.id, text="Mashinaning 4ta rasmini yuboring")
+        await put_step(cid=call.message.chat.id, step="get_photos")
+    
---
+++ keyboards/inline/user.py
@@ -0,0 +1,17 @@
+from aiogram.types import InlineKeyboardMarkup,InlineKeyboardButton
+
+def home_menu():
+    x = InlineKeyboardMarkup(row_width=2)
+    btn1 = InlineKeyboardButton(text="Kanalga e'lon joylash",callback_data="put_ads")
+    btn2 = InlineKeyboardButton(text="Biz haqimizda",callback_data="our_info")
+    btn3 = InlineKeyboardButton(text="Qo'llanma",callback_data="guide")
+    x.add(btn1)
+    x.add(btn2,btn3)
+    return x
+
+def ads_yes_no():
+    x = InlineKeyboardMarkup(row_width=2)
+    btn1 = InlineKeyboardButton(text="Ha",callback_data="yes_ads")
+    btn2 = InlineKeyboardButton(text="Yo'q",callback_data="no_ads")
+    x.add(btn1,btn2)
+    return x
