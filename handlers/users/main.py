a2cd38063165ed0792306344b3c2f2619c5243bb
root <root@vmi1949316.contaboserver.net>
2024-07-06 08:18:21 +0200

    new file:   .env.save 	modified:   handlers/users/main.py 	modified:   handlers/users/start.py

--- handlers/users/main.py
+++ handlers/users/main.py
@@ -1,4 +1,16 @@
 from aiogram import types
-
 from loader import dp
 
+from utils.db_api.alch import put_step
+from keyboards.inline.user import ads_yes_no
+
+@dp.callback_query_handler(text="put_ads")
+async def uz_show_apps(call: types.CallbackQuery):
+    await call.message.answer("E'lon joylamoqchimisiz?", reply_markup=ads_yes_no())
+    await call.answer(cache_time=60)
+
+@dp.callback_query_handler(text="yes_ads")
+async def uz_show_apps(call: types.CallbackQuery):
+    await call.message.answer("Mashinaning 4ta rasmini yuboring")
+    put_step(cid=call.message.chat.id, step="get_photos")
+    await call.answer(cache_time=60)
--- handlers/users/start.py
+++ handlers/users/start.py
@@ -10,14 +10,3 @@ from keyboards.inline.user import home_menu,ads_yes_no
 async def bot_start(message: types.Message):
     create_user(cid=message.chat.id)
     await message.answer(f"Salom, {message.from_user.full_name}!",reply_markup=home_menu())
-
-@dp.callback_query_handler(lambda call: True)
-async def get_ads(call: types.CallbackQuery):
-    print(call)
-    if call.data == "put_ads":
-        print("-----")
-        await bot.send_message(chat_id=call.message.chat.id, text="E'lon joylamoqchimisiz?", reply_markup=ads_yes_no())
-    elif call.data == "yes_ads":
-        await bot.send_message(chat_id=call.message.chat.id, text="Mashinaning 4ta rasmini yuboring")
-        await put_step(cid=call.message.chat.id, step="get_photos")
-    
